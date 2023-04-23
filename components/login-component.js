22e6cd5c6dfba22e9ef9dee59056da4c7d7602be
SSUHOY <sammysoul96@gmail.com>
2023-04-23 15:21:33 +0300

    Добавляет форму регистрации

--- components/login-component.js
+++ components/login-component.js
@@ -1,18 +1,33 @@
 import { loginUser } from "../api.js";
 
 export function renderLoginComponent({ appEl, setToken, fetchTodosAndRender }) {
+
+    let isLoginMode = false;
+
+
+const renderForm = () => {
+
     const appHtml = `<h1>Список задач</h1>
     <div class="form">
-    <h3 class="form-title">Форма входа</h3>
+    <h3 class="form-title">Форма ${isLoginMode ? 'входа' : 'регистрации'}</h3>
     <div class="form-row">
+
+    ${isLoginMode
+            ? ''
+            : 'Имя  <input type="text" id="name-input" class="input" /> <br><br>'}
         Логин
         <input type="text" id="login-input" class="input" />
+        <br />
         <br />
         Пароль
         <input type="password" id="password-input" class="input" />
     </div>
     <br />
-    <button class="button" id="login-button">Войти</button>
+    <button class="button" id="login-button">${isLoginMode ? 'Войти' : 'Зарегистрироваться'}</button>
+
+    <br />
+    <br />
+    <button class="button" id="toggle-button">Перейти ${isLoginMode ? 'к регистрации' : 'ко входу'}</button>
     </div>`
         ;
 
@@ -46,4 +61,10 @@ export function renderLoginComponent({ appEl, setToken, fetchTodosAndRender }) {
             alert(error.message);
         })
     });
+    document.getElementById('toggle-button').addEventListener('click', () => {
+        isLoginMode = !isLoginMode;
+        renderForm();
+    })
+}
+renderForm();
 }
